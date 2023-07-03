bd234d013417c05c4991a1cddbb592f5db248e97
João Guilherme <joaaogui@gmail.com>
2023-07-03 16:20:56 -0300

    fix: adds fixes

--- netlify/functions/phone-formatter/phone-formatter.js
+++ netlify/functions/phone-formatter/phone-formatter.js
@@ -1,13 +1,10 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
   try {
-    const subject = event.queryStringParameters.name || 'World'
+    const phone = event.body
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+      body: phone,
     }
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
